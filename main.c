2bc0144493ddfc87a5d9e54ac998e401057d3f13
gustafnilsson94@hotmail.com <gustafnilsson94@hotmail.com>
2025-05-08 17:44:29 +0200

    Done

--- main.c
+++ main.c
@@ -19,11 +19,53 @@ int main()
     for(int i = 0; i <= 100; i++)
     {
         diceRolls[i] = (rand() % 6) + 1;
-        printf("%d\n", &diceRolls[i]);
     }
 
+    int one = 0;
+    int two = 0;
+    int three = 0;
+    int four = 0;
+    int five = 0;
+    int six = 0;
 
-  
+    for(int i = 0; i <= 100; i++)
+    {
+        if(diceRolls[i] == 1)
+        {
+            one++;
+        }
+        if(diceRolls[i] == 2)
+        {
+            two++;
+        }
+        if(diceRolls[i] == 3)
+        {
+            three++;
+        }
+        if(diceRolls[i] == 4)
+        {
+            four++;
+        }
+        if(diceRolls[i] == 5)
+        {
+            five++;
+        }
+        if(diceRolls[i] == 6)
+        {
+            six++;
+        }
+        totalSum += diceRolls[i];
+    }
+    averageSum = totalSum / 100.0;
 
+    printf("%d\n", one);
+    printf("%d\n", two);
+    printf("%d\n", three);
+    printf("%d\n", four);
+    printf("%d\n", five);
+    printf("%d\n", six);
+    printf("%d\n", totalSum);
+    printf("%.1f\n", averageSum);
+    
     return 0;
 }
